d9b42614c255af25c82fbbf3c8c7c3ee84310fe4
Franklin Jhoel Patiño Chimbo <franklin.patino@epn.edu.ec>
2023-07-24 18:52:16 -0500

    Reconocimiento voz

---
+++ LLM/voz.py
@@ -0,0 +1,19 @@
+# Instalar las librerias necesarias
+
+import speech_recognition as sr
+import webbrowser as wb
+
+reconocimiento = sr.Recognizer()
+
+# Especificar el archivo de audio de entrada 
+
+with sr.Microphone() as source:
+    print("Hola en que puedo ayudarte?")
+    audio = reconocimiento.listen(source)
+
+# Imprimir el texto del audio
+
+print("Acabas de decir: ")
+frase = reconocimiento.recognize_google(audio, language='es-EC')
+print(frase)
+url="https://www.google.com/search?q="
